382e97704bdb00864c04e2fbf0a180e0fa72a55b
Bowen <chin_bo_wen@yahoo.com>
2015-07-21 14:22:36 +0800

    Initial commit

---
+++ app/src/main/java/com/bowenchin/android/materialapp/activity/FriendsFragment.java
@@ -0,0 +1,33 @@
+package com.bowenchin.android.materialapp.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bowenchin.android.materialapp.R;
+
+/**
+ * Created by bowenchin on 21/7/2015.
+ */
+public class FriendsFragment extends Fragment {
+
+
+    public FriendsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friends, container, false);
+    }
+}
